9b8503984c01f2fc1c41dde0decbf00eca941411
Yannick Francois <yaf@elsif.fr>
2021-07-23 11:41:50 +0200

    compte le nombre de chiffre

--- spec/SortSpec.js
+++ spec/SortSpec.js
@@ -1,17 +1,37 @@
 describe("Sort", function() {
 
-  it("[1] renvoi [1]", function (){
-    expect(tri([1])).toEqual([1]);
+  describe("Tri", function() {
+    it("[1] renvoi [1]", function (){
+      expect(tri([1])).toEqual([1]);
+    })
+
+    it("[4] renvoi [4]", function (){
+      expect(tri([4])).toEqual([4]);
+    })
+
+    it("[4, 7, 7] renvoi [7, 7, 4]", function (){
+      expect(tri([4, 7, 7])).toEqual([7, 7, 4]);
+      pending()
+    })
+
+    it("test de recette", function() {
+      expect(tri([1, 2 ,2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 8, 9, 10])).toEqual([5, 5, 5, 5, 3, 3, 3, 6, 6, 6, 2, 2, 4, 4, 1, 7, 8, 9, 10]);
+      pending()
+    });
   })
 
-  it("[4] renvoi [4]", function (){
-    expect(tri([4])).toEqual([4]);
+  describe("countNumber", function() {
+    it("[1] renvoie {1: 1}", function() {
+      expect(countNumber([1])).toEqual({1: 1})
+    })
+
+    it("[4] renvoie {4: 1}", function() {
+      expect(countNumber([4])).toEqual({4: 1})
+    })
+
+    it("[4, 4] renvoie {4: 2}", function() {
+      expect(countNumber([4, 4])).toEqual({4: 2})
+    })
   })
-
-
-  it("test de recette", function() {
-    expect(tri([1, 2 ,2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 8, 9, 10])).toEqual([5, 5, 5, 5, 3, 3, 3, 6, 6, 6, 2, 2, 4, 4, 1, 7, 8, 9, 10]);
-    pending()
-  });
 
 });
--- src/Sort.js
+++ src/Sort.js
@@ -1,4 +1,31 @@
 
 const tri = (numbers) => {
+  if (numbers.length == 1) {
+    return numbers
+  }
+
+  numberCounter = countNumber(numbers)
+  // numbers.sort
+  // {
+  //  1 : 1 fois 
+  //  2 : 4 fois 
+  //  5 : 7 fois
+  //}
   return numbers
+}
+
+const countNumber = (numbers) => {
+  resultat = {}
+  
+  numbers.forEach(function (number) {
+    
+    if (resultat[number] >= 0){
+      resultat[number] += 1
+    } else {
+      resultat[number] = 1
+    }
+
+  })
+  
+  return resultat
 }
